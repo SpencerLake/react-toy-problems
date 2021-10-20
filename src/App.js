d9a8586d36031e65a8e0d07a5b9522652134b42b
Spencer-Lake <spencer.m.lake11@gmail.com>
2021-10-20 19:17:55 -0400

    finished step 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { render } from 'react-dom';
 import React, { Component } from 'react';
+import TopicBrowser from './components/TopicBrowser/TopicBrowser.js';
 
 class App extends Component {
   render() {
-    
+    render(TopicBrowser);
   }
-}
+};
 
 export default App;
---
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+class TopicBrowser extends Component {
+    render() {
+        return 
+            <p>Hello World.</p>
+    }
+}
+
+export default TopicBrowser;
