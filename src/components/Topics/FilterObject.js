bf0e02d059835d920c9e5472625663dcd46c7b5a
Spencer-Lake <spencer.m.lake11@gmail.com>
2021-10-21 18:40:30 -0400

    started problem 4

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,9 +1,41 @@
 import React, { Component } from "react";
 
 export default class FilterObject extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            unFilteredArray: [
+                {id: 1, brand: "Vear", powerSource: "Quartz", size: 36},
+                {id: 2, brand: "Citizen", powerSource: "Solar", size: 40},
+                {id: 3, brand: "Burtucci", powerSource: "Quartz", size: 36}
+            ],
+            userInput: "",
+            filteredArray: []
+        }
+    };
+
+    handleChange(change) {
+        this.setState({ userInput: change })
+    }
+
+    handleClick(input) {
+
+    }
+
     render() {
+        
         return (
-            <p>FilterObject</p>
+            <div className="puzzleBox filterObjectPB">
+                <h4>Filter Object</h4>
+                <span className="puzzleText">UnFiltered Array</span>
+                <input 
+                className="inputLine" 
+                onChange={(e) => {this.handleChange(e.target.value)}}/>
+                <button 
+                className="confirmationButton"
+                onClick={() => {this.handleChange(this.state.userInput)}}>Enter</button>
+                <span className="resultsBox filterObjectRB">Filtered Array</span>
+            </div>
         )
     };
 };
